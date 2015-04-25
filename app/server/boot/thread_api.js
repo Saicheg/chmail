2b3f9d1b75ae6c145b11a4449e727dcf332f2d38
xputnikx <xputnikx@gmail.com>
2015-04-26 02:05:42 +0300

    node piu piu

--- app/server/boot/thread_api.js
+++ app/server/boot/thread_api.js
@@ -2,7 +2,9 @@
  * Created by vladimirhudnitsky on 4/26/15.
  */
 
-var Gmail = require('node-gmail-api');
+var google = require('googleapis');
+var config = require('../../providers.json');
+var gmail = google.gmail('v1');
 module.exports = function(app) {
     app.get('/mythreads/:identiryId', function(req, res) {
         var id = req.params.identiryId;
@@ -10,12 +12,18 @@ module.exports = function(app) {
             if(err || !instance){
                 res.send(err);
             }else {
-                var gmail = new Gmail(instance.__data.credentials.accessToken);
-                var threads = gmail.threads('', {});
-                threads.on('data', function (d) {
-                    console.log(d.snippet);
-                    res.send("ok");
-                })
+
+                var OAuth2 = google.auth.OAuth2;
+                var oauth2Client = new OAuth2(config["google-login"].clientID, config["google-login"].clientSecret, config["google-login"].callbackURL);
+
+                oauth2Client.setCredentials({
+                    access_token: instance.__data.credentials.accessToken,
+                    refresh_token: instance.__data.credentials.accessToken
+                });
+
+                gmail.users.threads.list({"auth" : oauth2Client, "userId" : "me"}, function(err, response){
+                    console.log('Result: ' + (err ? err.message : response));
+                });
             }
         });
     });
