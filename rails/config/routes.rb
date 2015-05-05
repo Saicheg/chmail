f9c767c4cebf53b98e4ff77670dcc6acec5e46af
Valentine Zavadsky <saicheg@gmail.com>
2015-05-05 20:02:06 +0300

    draft version of google omniauth

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'landings#index'
-  get '/chats', to: 'visitors#index'
-  get '/send_mail', to: 'visitors#send_mail'
+
+  # get '/chats', to: 'visitors#index'
+  # get '/send_mail', to: 'visitors#send_mail'
+
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 end
