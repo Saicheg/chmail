827c78a49d3c1ed963b35ca29c5488d041f0a9e3
Valentine Zavadsky <saicheg@gmail.com>
2015-05-05 20:53:35 +0300

    implement simple google oauth login

--- rails/app/controllers/users/omniauth_callbacks_controller.rb
+++ rails/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,47 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def google_oauth2
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    binding.pry
 
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
+    if user.present?
+      update_user_credentials
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      create_user
+    end
+
+    sign_in(user)
+
+    redirect_to root_url
+  end
+
+  def user
+    @user ||= User.find_by(email: google_data.info.email)
+  end
+
+  def google_data
+    @google_data ||= request.env["omniauth.auth"]
+  end
+
+  def create_user
+    @user = User.create! do |fields|
+      fields.email = google_data.info.email
+      fields.password = SecureRandom.hex
+      fields.password_confirmation = fields.password
+      fields.token = google_data.credentials.token
+      fields.refresh_token = google_data.credentials.refresh_token
+      fields.expires_at = Time.at(google_data.credentials.expires_at)
+      fields.uid = google_data.uid
+      fields.name = google_data.info.name
+      fields.first_name = google_data.info.first_name
+      fields.last_name = google_data.info.last_name
+      fields.image = google_data.info.image
+      fields.gender = google_data.info.gender
     end
   end
+
+  def update_user_credentials
+    user.update_attributes! token: google_data.credentials.token,
+                            refresh_token: google_data.credentials.refresh_token,
+                            expires_at:  Time.at(google_data.credentials.expires_at)
+  end
+
 end
